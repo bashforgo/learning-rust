c04267188a846d2cb1b57d0128c6071141d9812a
Barnabas Forgo <speedo@devo.hu>
2019-06-10 08:27:29 +0100

    step 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+    println!("Enter the number");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read_line");
+
+    println!("Your guess: {}", guess);
 }
