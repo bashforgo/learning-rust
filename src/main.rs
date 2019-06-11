ed5fb24bd6914d019a504b86f7f7778bf988b86a
Barnabas Forgo <speedo@devo.hu>
2019-06-11 21:43:57 +0100

    fib

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,12 @@ fn main() {
         .as_ref()
     {
         "convert" => convert(),
+        "fib" => fib(),
         program => panic!("unknown program `{}`", program),
     }
 }
 
+//#region convert
 fn convert() {
     let help = "-f <deg> to convert from F; -c <deg> to convert from C";
     match args().nth(2).expect(help).as_ref() {
@@ -20,18 +22,47 @@ fn convert() {
     };
 }
 
-fn parse_third() -> f32 {
+fn convert_parse() -> f32 {
     let err = "please pass a number";
     let str = args().nth(3).expect(err);
     str.parse().expect(err)
 }
 fn from_f() {
-    let f = parse_third();
+    let f = convert_parse();
     let c = (f - 32.0) * 5.0 / 9.0;
     println!("{} F is {:.2} C", f, c);
 }
 fn from_c() {
-    let c = parse_third();
+    let c = convert_parse();
     let f = c * 9.0 / 5.0 + 32.0;
     println!("{} C is {:.2} F", c, f);
 }
+//#endregion
+
+//#region fib
+fn fib() {
+    let nth = fib_parse();
+
+    let fib = match nth {
+        1 | 2 => 1,
+        _ => {
+            let mut cache: (i128, i128) = (1, 1);
+            for _ in 2..nth {
+                cache = (cache.1, cache.0 + cache.1)
+            }
+
+            cache.1
+        }
+    };
+    println!("the nth fib is {} where n={}", fib, nth);
+}
+fn fib_parse() -> i32 {
+    let err = "please pass a positive integer";
+    let str = args().nth(2).expect(err);
+    let i = str.parse().expect(err);
+    if i < 1 {
+        panic!(err)
+    }
+    i
+}
+//#endregion
