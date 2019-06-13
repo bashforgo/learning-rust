b246b41c46d3b77f29e468904d62d35e6263f48e
Barnabas Forgo <speedo@devo.hu>
2019-06-13 07:19:03 +0100

    borrowing function

--- chapter-05/src/main.rs
+++ chapter-05/src/main.rs
@@ -1,3 +1,17 @@
+#[derive(Debug)]
+struct Rect {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rect = Rect {
+        width: 12,
+        height: 34,
+    };
+    println!("the rect: {:?}, the area: {}", rect, area(&rect));
+}
+
+fn area(rect: &Rect) -> u32 {
+    rect.width * rect.height
 }
