9d6b7001199a93816bd40ef327ca865834314944
Barnabas Forgo <speedo@devo.hu>
2019-06-13 07:41:19 +0100

    mutable borrow and take ownership

--- chapter-05/src/main.rs
+++ chapter-05/src/main.rs
@@ -4,14 +4,27 @@ struct Rect {
     height: u32,
 }
 
+impl Rect {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn grow(&mut self) {
+        self.width += 1;
+        self.height += 1;
+    }
+
+    fn invalidate(self) {}
+}
+
 fn main() {
-    let rect = Rect {
+    let mut rect = Rect {
         width: 12,
         height: 34,
     };
-    println!("the rect: {:?}, the area: {}", rect, area(&rect));
-}
-
-fn area(rect: &Rect) -> u32 {
-    rect.width * rect.height
+    println!("the rect: {:?}, the area: {}", rect, rect.area());
+    rect.grow();
+    println!("new rect: {:?}, area: {}", rect, rect.area());
+    rect.invalidate();
+    // println!("new rect: {:?}, area: {}", rect, rect.area());
 }
