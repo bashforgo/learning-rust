419df78451118844e7037d88b4e9e64b4899180b
Barnabas Forgo <speedo@devo.hu>
2019-06-21 00:37:49 +0100

    read arguments

--- chapter12/src/main.rs
+++ chapter12/src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let needle = args.get(1).unwrap_or_else(die("first argument should be a regex"));
+    let haystack = args.get(2).unwrap_or_else(die("second argument should be a file"));
+
+    println!("find {} in {}", needle, haystack);
+}
+
+fn die<T>(msg: &'static str) -> impl FnOnce() -> T {
+    return move || {
+        eprintln!("{}", msg);
+        process::exit(1);
+    };
 }
