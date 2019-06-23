fd4586ccb0f871f9da90742e5e5c8e03afc658e0
Barnabas Forgo <speedo@devo.hu>
2019-06-23 21:24:02 +0100

    generic memoizer

--- chapter13/src/main.rs
+++ chapter13/src/main.rs
@@ -1,3 +1,48 @@
+use std::cmp::Eq;
+use std::collections::HashMap;
+use std::hash::Hash;
+
+struct Memo<T: Eq + Hash + Clone, U> {
+    computation: Box<Fn(&T) -> U>,
+    cache: HashMap<T, U>,
+}
+
+impl<T: Eq + Hash + Clone, U> Memo<T, U> {
+    fn new(computation: Box<Fn(&T) -> U>) -> Memo<T, U> {
+        Memo {
+            computation,
+            cache: HashMap::new(),
+        }
+    }
+
+    fn get(&mut self, k: T) -> &U {
+        let cache = &mut self.cache;
+        let computation = &self.computation;
+
+        let klone = k.clone();
+
+        cache.entry(k).or_insert_with(|| (computation)(&klone))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut memo = Memo::new(Box::new(|param: &String| {
+        println!("expensive computation {}", param);
+        format!("result {}", param)
+    }));
+
+    let res = memo.get(String::from("ayy"));
+    println!("{:?}", res);
+
+    let res = memo.get(String::from("lmao"));
+    println!("{:?}", res);
+
+    let res = memo.get(String::from("wow"));
+    println!("{:?}", res);
+
+    let res = memo.get(String::from("ayy"));
+    println!("{:?}", res);
+
+    let res = memo.get(String::from("wow"));
+    println!("{:?}", res);
 }
