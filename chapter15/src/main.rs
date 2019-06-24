1cbf59700276b38711441fce061b8befaf5bf6b7
Barnabas Forgo <speedo@devo.hu>
2019-06-24 18:37:20 +0100

    cons list

--- chapter15/src/main.rs
+++ chapter15/src/main.rs
@@ -1,3 +1,51 @@
+use std::fmt;
+
+enum List<T> {
+    Cons(T, Box<List<T>>),
+    Nil,
+}
+
+use List::*;
+
+impl<T> List<T> {
+    fn cons(v: T, rest: List<T>) -> List<T> {
+        Cons(v, Box::new(rest))
+    }
+}
+
+impl<T: fmt::Display> fmt::Debug for List<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "List[")?;
+
+        let mut first = true;
+        let mut current = self;
+        loop {
+            match current {
+                Cons(e, rest) => {
+                    if first {
+                        first = false;
+                    } else {
+                        write!(f, ", ")?;
+                    }
+                    write!(f, "{}", e)?;
+                    current = rest;
+                }
+                Nil => {
+                    break;
+                }
+            }
+        }
+
+        write!(f, "]")
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cons = List::cons;
+    println!("{:?}", cons(1, cons(2, cons(3, cons(4, Nil)))));
+    let cons = List::cons;
+    println!(
+        "{:?}",
+        cons("ayy", cons("lmao", cons("such", cons("list", Nil))))
+    );
 }
