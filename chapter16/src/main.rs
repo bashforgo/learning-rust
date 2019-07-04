0008d7973e4b4302bc601d6c93315bf615b2ab94
Barnabas Forgo <speedo@devo.hu>
2019-07-04 17:10:11 +0100

    deadlock

--- chapter16/src/main.rs
+++ chapter16/src/main.rs
@@ -1,17 +1,34 @@
 use std::sync::{Arc, Mutex};
 use std::thread;
+use std::time::Duration;
 
 fn main() {
-    let counter = Arc::new(Mutex::new(0));
+    let mutex_a = Arc::new(Mutex::new(0));
+    let mutex_b = Arc::new(Mutex::new(0));
     let mut handles = vec![];
 
-    for i in 0..10 {
-        let counter = Arc::clone(&counter);
+    {
+        let mutex_a = Arc::clone(&mutex_a);
+        let mutex_b = Arc::clone(&mutex_b);
         let handle = thread::spawn(move || {
-            let mut num = counter.lock().unwrap();
+            println!("thread a starting");
+            let _a = mutex_a.lock().unwrap();
+            thread::sleep(Duration::from_secs(1));
+            let _b = mutex_b.lock().unwrap();
+            println!("thread a done");
+        });
+        handles.push(handle);
+    }
 
-            *num += 1;
-            println!("thread {} done", i);
+    {
+        let mutex_a = Arc::clone(&mutex_a);
+        let mutex_b = Arc::clone(&mutex_b);
+        let handle = thread::spawn(move || {
+            println!("thread b starting");
+            let _b = mutex_b.lock().unwrap();
+            thread::sleep(Duration::from_secs(1));
+            let _a = mutex_a.lock().unwrap();
+            println!("thread b done");
         });
         handles.push(handle);
     }
@@ -20,5 +37,5 @@ fn main() {
         handle.join().unwrap();
     }
 
-    println!("Result: {}", *counter.lock().unwrap());
+    println!("done");
 }
