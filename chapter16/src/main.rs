6653bc5199664c26b90a613ceedb310e93d1e8d0
Barnabas Forgo <speedo@devo.hu>
2019-07-04 16:57:24 +0100

    mutex playground

--- chapter16/src/main.rs
+++ chapter16/src/main.rs
@@ -1,3 +1,24 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for i in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+            println!("thread {} done", i);
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
