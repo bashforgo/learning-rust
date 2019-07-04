5952b45af74b2bf912186878e5e1a82b21e9b8c5
Barnabas Forgo <speedo@devo.hu>
2019-07-04 22:34:13 +0100

    quicksort

---
+++ chapter16/src/deadlock.rs
@@ -0,0 +1,41 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+fn deadlock() {
+    let mutex_a = Arc::new(Mutex::new(0));
+    let mutex_b = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    {
+        let mutex_a = Arc::clone(&mutex_a);
+        let mutex_b = Arc::clone(&mutex_b);
+        let handle = thread::spawn(move || {
+            println!("thread a starting");
+            let _a = mutex_a.lock().unwrap();
+            thread::sleep(Duration::from_secs(1));
+            let _b = mutex_b.lock().unwrap();
+            println!("thread a done");
+        });
+        handles.push(handle);
+    }
+
+    {
+        let mutex_a = Arc::clone(&mutex_a);
+        let mutex_b = Arc::clone(&mutex_b);
+        let handle = thread::spawn(move || {
+            println!("thread b starting");
+            let _b = mutex_b.lock().unwrap();
+            thread::sleep(Duration::from_secs(1));
+            let _a = mutex_a.lock().unwrap();
+            println!("thread b done");
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("done");
+}
--- chapter16/src/main.rs
+++ chapter16/src/main.rs
@@ -1,41 +1,58 @@
-use std::sync::{Arc, Mutex};
-use std::thread;
-use std::time::Duration;
+type Value = i32;
+
+fn split_first(values: Vec<Value>) -> (Value, Vec<Value>) {
+    let mut first = true;
+    let mut result = (0, vec![]);
+
+    for v in values.into_iter() {
+        if first {
+            first = false;
+            result.0 = v
+        } else {
+            result.1.push(v);
+        }
+    }
+
+    result
+}
+
+fn partition<Predicate>(values: Vec<Value>, pred: Predicate) -> (Vec<Value>, Vec<Value>)
+where
+    Predicate: Fn(&Value) -> bool,
+{
+    let mut result = (vec![], vec![]);
+
+    for v in values {
+        let result = if pred(&v) {
+            &mut result.0
+        } else {
+            &mut result.1
+        };
+        result.push(v);
+    }
+
+    result
+}
+
+fn quick_sort(values: Vec<Value>) -> Vec<Value> {
+    if values.len() <= 1 {
+        values
+    } else {
+        let (pivot, rest) = split_first(values);
+        let (left, right) = partition(rest, |v| *v < pivot);
+
+        let mut result: Vec<Value> = quick_sort(left);
+        result.push(pivot);
+        result.append(&mut quick_sort(right));
+
+        result
+    }
+}
 
 fn main() {
-    let mutex_a = Arc::new(Mutex::new(0));
-    let mutex_b = Arc::new(Mutex::new(0));
-    let mut handles = vec![];
-
-    {
-        let mutex_a = Arc::clone(&mutex_a);
-        let mutex_b = Arc::clone(&mutex_b);
-        let handle = thread::spawn(move || {
-            println!("thread a starting");
-            let _a = mutex_a.lock().unwrap();
-            thread::sleep(Duration::from_secs(1));
-            let _b = mutex_b.lock().unwrap();
-            println!("thread a done");
-        });
-        handles.push(handle);
-    }
-
-    {
-        let mutex_a = Arc::clone(&mutex_a);
-        let mutex_b = Arc::clone(&mutex_b);
-        let handle = thread::spawn(move || {
-            println!("thread b starting");
-            let _b = mutex_b.lock().unwrap();
-            thread::sleep(Duration::from_secs(1));
-            let _a = mutex_a.lock().unwrap();
-            println!("thread b done");
-        });
-        handles.push(handle);
-    }
-
-    for handle in handles {
-        handle.join().unwrap();
-    }
-
-    println!("done");
+    let v = vec![
+        235, 25, 253, 23, 414, 14, 12, 431, 24, 13, 543, 63, 647, 568, 67, 969, 85, 47535, 6243,
+    ];
+    let s = quick_sort(v);
+    println!("{:?}", s);
 }
