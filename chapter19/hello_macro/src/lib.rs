5e3a4f377bfbbb420674c83bb83f38efa43259ab
Barnabas Forgo <speedo@devo.hu>
2019-07-11 08:58:15 +0100

    basic call tracing macro

--- chapter19/hello_macro/src/lib.rs
+++ chapter19/hello_macro/src/lib.rs
@@ -1,8 +1,8 @@
 extern crate proc_macro;
 
 use crate::proc_macro::TokenStream;
-use quote::quote;
-use syn;
+use quote::{quote, ToTokens};
+use syn::{self, ArgCaptured, Block, FnArg, ItemFn, Pat, PatIdent};
 
 #[proc_macro_derive(HelloMacro)]
 pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
@@ -24,4 +24,49 @@ fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
         }
     };
     gen.into()
+}
+
+#[proc_macro_attribute]
+pub fn hello_trace(attr: TokenStream, input: TokenStream) -> TokenStream {
+    if !attr.is_empty() {
+        panic!("no parameters needed")
+    }
+    let the_fn: ItemFn = syn::parse(input).unwrap();
+    let fn_block = the_fn.block.clone();
+    let mut gen = the_fn.clone();
+    let fn_ident = the_fn.ident.clone();
+    let mut input_idents = vec![];
+    for input in the_fn.decl.inputs.clone() {
+        match input {
+            FnArg::Captured(ArgCaptured {
+                pat: Pat::Ident(PatIdent { ident, .. }),
+                ..
+            }) => input_idents.push(ident),
+            _ => unimplemented!("input matching"),
+        }
+    }
+    let input_idents2 = input_idents.clone();
+    let new_block = quote! {
+        {
+            let __trace_name = stringify!(#fn_ident);
+            print!("[debug {}]", __trace_name);
+            #(
+                print!(" {}: {:?}", stringify!(#input_idents), #input_idents2);
+            )*
+            println!();
+            let __trace_start = std::time::Instant::now();
+            let __trace_returns = #fn_block;
+            let __trace_end = std::time::Instant::now();
+            println!(
+                "[debug {}] return: {:?} (took {:?})",
+                __trace_name,
+                __trace_returns,
+                __trace_end - __trace_start,
+            );
+            __trace_returns
+        }
+    };
+    let new_block: Block = syn::parse2(new_block).unwrap();
+    gen.block = Box::new(new_block);
+    gen.into_token_stream().into()
 }
--- chapter19/src/main.rs
+++ chapter19/src/main.rs
@@ -1,9 +1,49 @@
 use chapter19::HelloMacro;
-use hello_macro::HelloMacro;
+use hello_macro::{hello_trace, HelloMacro};
 
 #[derive(HelloMacro)]
 struct MrsPancakes;
 
+#[hello_trace]
+fn add(left: i32, right: i32) -> i32 {
+    left + right
+}
+
+// #[hello_trace]
+fn add_tuple((left, right): (i32, i32)) -> i32 {
+    left + right
+}
+
+// #[hello_trace]
+fn add_debug(left: i32, right: i32) -> i32 {
+    print!("[debug add]");
+    print!(" left: {:?}", left);
+    print!(" right: {:?}", right);
+    println!("");
+    let __debug_start = std::time::Instant::now();
+    let __debug_returns = { left + right };
+    let __debug_end = std::time::Instant::now();
+    println!(
+        "[debug add] return: {:?} (took {:?})",
+        __debug_returns,
+        __debug_end - __debug_start
+    );
+    __debug_returns
+}
+
+#[hello_trace]
+fn naive_fib(nth: u32) -> i32 {
+    if nth <= 1 {
+        1
+    } else {
+        naive_fib(nth - 1) + naive_fib(nth - 2)
+    }
+}
+
 fn main() {
-    MrsPancakes::hello_macro();
+    // MrsPancakes::hello_macro();
+    // add(12, 24);
+    // add_tuple((12, 24));
+    // add_debug(12, 24);
+    println!("{:?}", naive_fib(30));
 }
