b9fc83953950e8478692b31111690ff839268f4f
Barnabas Forgo <speedo@devo.hu>
2019-07-18 08:47:48 +0100

    graceful shutdown

--- chapter20/src/bin/main.rs
+++ chapter20/src/bin/main.rs
@@ -11,7 +11,7 @@ fn main() {
 
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
         pool.exec(|| {
--- chapter20/src/lib.rs
+++ chapter20/src/lib.rs
@@ -3,14 +3,14 @@ use std::thread::{self, JoinHandle};
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    queue: mpsc::Sender<Job>,
+    queue: mpsc::Sender<Message>,
 }
 
 impl ThreadPool {
     pub fn new(n_threads: usize) -> ThreadPool {
         assert!(n_threads > 0);
 
-        let (tx, rx) = mpsc::channel::<Job>();
+        let (tx, rx) = mpsc::channel();
         let rx = Arc::new(Mutex::new(rx));
         let workers = (0..n_threads)
             .into_iter()
@@ -24,42 +24,55 @@ impl ThreadPool {
     where
         T: FnOnce() + Send + 'static,
     {
-        self.queue
-            .send(Job {
-                task: Box::new(task),
-            })
-            .unwrap()
+        self.queue.send(Message::Job(Job(Box::new(task)))).unwrap()
     }
 }
 
-type Jobs = Arc<Mutex<mpsc::Receiver<Job>>>;
-struct Worker {
-    id: usize,
-    thread: JoinHandle<()>,
-}
-
-impl Worker {
-    fn new(id: usize, jobs: Jobs) -> Worker {
-        Worker {
-            id,
-            thread: thread::spawn(move || loop {
-                let job = {
-                    let lock = jobs.lock();
-                    let rx = &*lock.unwrap();
-                    rx.recv().unwrap()
-                };
-                job.exec();
-            }),
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for _ in &self.workers {
+            self.queue.send(Message::Terminate).unwrap();
+        }
+        for worker in &mut self.workers {
+            worker.thread.take().unwrap().join().unwrap();
         }
     }
 }
 
-struct Job {
-    task: Box<FnOnce() + Send + 'static>,
+enum Message {
+    Job(Job),
+    Terminate,
 }
+type Jobs = Arc<Mutex<mpsc::Receiver<Message>>>;
+struct Worker {
+    _id: usize,
+    thread: Option<JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(_id: usize, jobs: Jobs) -> Worker {
+        Worker {
+            _id,
+            thread: Some(thread::spawn(move || loop {
+                let message = {
+                    let lock = jobs.lock();
+                    let rx = &*lock.unwrap();
+                    rx.recv().unwrap()
+                };
+
+                match message {
+                    Message::Job(job) => job.exec(),
+                    Message::Terminate => break,
+                }
+            })),
+        }
+    }
+}
+
+struct Job(Box<FnOnce() + Send + 'static>);
 
 impl Job {
     fn exec(self) {
-        (self.task)();
+        (self.0)();
     }
 }
